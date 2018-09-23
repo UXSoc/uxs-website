28650a858c26e278edf6f22ba1ae6dc0ff6205b1
Xavi Ablaza <xlablaza@gmail.com>
2018-09-22 22:14:44 -0700

    Setup actionmailer for dev and prod

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,10 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :sendgrid_actionmailer
+  config.action_mailer.sendgrid_actionmailer_settings = {
+    api_key: ENV['SENDGRID_API_KEY']
+  }
 end
