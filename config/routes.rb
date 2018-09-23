26e2cba522c00b45eb0a9329df3b1ff2463e31ec
Xavi Ablaza <xlablaza@gmail.com>
2018-09-23 00:03:14 -0700

    Configure routes for user

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
+
+  devise_scope :user do
+    root "devise/sessions#new"
+  end
 end
