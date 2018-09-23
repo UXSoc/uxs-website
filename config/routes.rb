43da23685e57b6958b089066beab8efd20c6a5b4
Xavi Ablaza <xlablaza@gmail.com>
2018-09-23 00:14:46 -0700

    Add initial dashboard template

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
   devise_for :users
 
   devise_scope :user do
-    root "devise/sessions#new"
+    authenticated do
+      root "dashboard#show"
+    end
+
+    unauthenticated do
+      root "devise/sessions#new"
+    end
   end
 end
