7ae04a7dc6d238e8752af4c4429184ef35f9f18a
Xavi Ablaza <xlablaza@gmail.com>
2018-09-23 21:27:37 -0700

    Configure webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,20 @@
-/* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
+import '../application.scss'
+import Rails from 'rails-ujs'
+import Turbolinks from 'turbolinks'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { library, dom } from '@fortawesome/fontawesome-free'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { Application } from 'stimulus'
+import { definitionsFromContext } from 'stimulus/webpack-helpers'
 
-console.log('Hello World from Webpacker')
+const application = Application.start()
+const context = require.context('./controllers', true, /\.js$/)
+
+Rails.start()
+Turbolinks.start()
+application.load(definitionsFromContext(context))
+
+document.addEventListener('turbolinks:load', () => {
+  library.add(fab, far, fas)
+})
