e2db9001522ec7fb5acd385afad2682a7ce7865f
Xavi Ablaza <xlablaza@gmail.com>
2018-09-23 22:22:32 -0700

    Make FontAwesome render properly

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@ import '../application.scss'
 import Rails from 'rails-ujs'
 import Turbolinks from 'turbolinks'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-import { library, dom } from '@fortawesome/fontawesome-free'
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
 import { far } from '@fortawesome/free-regular-svg-icons'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { Application } from 'stimulus'
@@ -17,4 +17,5 @@ application.load(definitionsFromContext(context))
 
 document.addEventListener('turbolinks:load', () => {
   library.add(fab, far, fas)
+  dom.i2svg()
 })
